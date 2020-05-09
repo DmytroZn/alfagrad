faeebe912c0a2c12ba2a763522dcd89853710675
dmytrozn <dmytro.zn@gmail.com>
2020-05-09 19:59:46 +0300

    add robots.txt function and some chenges

--- alfagrad/main/urls.py
+++ alfagrad/main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('view/<str:pk>/', views.view, name='view'),
     path('about-us/', views.about_us, name='about_us'),
     path('all-projects/', views.all_projects, name='all_projects'),
+    path('robots.txt', views.robots_txt, name='robots_txt'),
 
 ]
--- alfagrad/main/views.py
+++ alfagrad/main/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from .models import LastProjects, BaseLevel, Location, Contacts
 
-
+from django.http import HttpResponse
+from django.views.decorators.http import require_GET
 
 # Create your views here.
 
 def main(request):
-    title = 'Альфаград, заборы'
+    title = 'Установка заборов под ключ | Альфаград'
+    contact_phone = Contacts.objects.first()
+    description = f'Заборы под ключ, установка заборов. Строительство, ремонт, монтаж. Киев и киевская область. Качественно не дорого. Звоните {contact_phone.phone}'
     obj = LastProjects.objects.all()
     base_level = BaseLevel.objects.first()
     location = Location.objects.first()
@@ -18,13 +21,16 @@ def main(request):
         'location':location,
         'contacts':contacts,
         'title':title,
+        'description':description
         }
     return render(request, template, context)
 
 
 def view(request, pk):
-    title = 'заборы не дорого, Киев'
     obj = LastProjects.objects.get(title=pk)
+    title = f'{obj.title} - установка, монтаж и хорошее качество | Альфаград'
+    contact_phone = Contacts.objects.first()
+    description = f'{obj.title} - установка заборов под ключ, ремонт, монтаж. Киев и киевская область. Качественно не дорого. Звоните {contact_phone.phone}'
     location = Location.objects.first()
     contacts = Contacts.objects.all()
     template = 'main/view.html'
@@ -33,12 +39,15 @@ def view(request, pk):
         'location':location,
         'contacts':contacts,
         'title':title,
+        'description':description,
 
         }
     return render(request, template, context)
 
 def about_us(request):
-    title = 'заборы не дорого, Киев'
+    title = 'Строительстро, монтаж, заборы, не дорого | Альфаград'
+    contact_phone = Contacts.objects.first()
+    description = f'Заборы под ключ, установка заборов, ремонт, монтаж. Киев и киевская область. Качественно не дорого. Звоните {contact_phone.phone}'
     base_level = BaseLevel.objects.first()
     location = Location.objects.first()
     contacts = Contacts.objects.all()
@@ -48,12 +57,15 @@ def about_us(request):
         'location':location,
         'contacts':contacts,
         'title':title,
+        'description':description,
 
         }
     return render(request, template, context)
 
 def all_projects(request):
-    title = 'заборы не дорого, Киев'
+    title = 'Заборы не дорого | Альфаград'
+    contact_phone = Contacts.objects.first()
+    description = f'Заборы под ключ, установка заборов, ремонт, монтаж. Киев и киевская область. Качественно не дорого. Звоните {contact_phone.phone}'
     objs = LastProjects.objects.all()
     location = Location.objects.first()
     contacts = Contacts.objects.all()
@@ -63,9 +75,21 @@ def all_projects(request):
         'location':location,
         'contacts':contacts,
         'title':title,
+        'description':description,
 
         }
     return render(request, template, context)
 
 
+
+@require_GET
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Disallow:"
+        # "User-Agent: *",
+        # "Disallow: /private/",
+        # "Disallow: /junk/",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
 
